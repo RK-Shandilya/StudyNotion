27cb8b9d04e8adc8b6e5801f387d79c16d20c9e9
RK-Shandilya <89962501+RK-Shandilya@users.noreply.github.com>
2023-07-17 04:57:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-@Crossorigin
 const express = require("express");
 const app = express();
 
@@ -22,12 +21,14 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({credentials:true,
-       origin: '*',
-       }
-    )
-)
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 app.use(
 	fileUpload({
